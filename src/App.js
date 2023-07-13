09c2e5a0743b52307f3e8dbf49d9a75063794056
Tsehay Woldekidan <124805364+tsehay2728@users.noreply.github.com>
2023-07-13 17:45:34 -0400

    netlify-clone

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+import Row from "./Components/Row/Row";
+import requests from "./Components/requests";
+import Banner from "./Components/Banner/Banner";
+import Nav from "./Components/Nav/Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated Movie" fetchUrl={requests.fetchTopRatedMovies} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+function Nav() {
+  const [show, handleShow] = useState(false);
+  
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    // return () => {
+    //   window.removeEventListener("scroll",null);
+    // };
+  }, []);
+
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
+        alt="Avatar logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
